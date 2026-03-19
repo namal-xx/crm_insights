1694723437b3bad1ccf301728e8855274f4a7ccd
namalrizwan <namalrizwan4@gmail.com>
2026-03-20 02:27:25 +0500

    Setting up README.md

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import base64
 import streamlit as st
 from streamlit_option_menu import option_menu
-import google.generativeai as genai
 
-
-genai.configure(api_key = st.secrets["GEMINI_API_KEY"]) # telling the google that i've acces to your api by providing api key
 
 
 st.set_page_config(page_title="CRM Insights", page_icon="📊")
